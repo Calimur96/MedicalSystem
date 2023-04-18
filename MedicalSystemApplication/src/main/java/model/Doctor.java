12875f3182445311e02d14eda1b74cf6b58c1029
toylep <toylenium@mail.ru>
2023-04-18 21:17:29 +0800

    Refactor Doctor Model

--- MedicalSystemApplication/src/main/java/model/Doctor.java
+++ MedicalSystemApplication/src/main/java/model/Doctor.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -18,30 +19,34 @@ public class Doctor extends User
 {
 	@Column(name = "type", nullable = true)
 	private String type;
-		
+
 	@Column(name = "shiftStart", nullable = true)
-    private Date shiftStart;
-	
-    @Column(name = "shiftEnd", nullable = true)
-    private Date shiftEnd;
-    
-    @Column(name = "averageRating", nullable = true)
-    private float averageRating;
+	private Date shiftStart;
+
+	@Column(name = "shiftEnd", nullable = true)
+	private Date shiftEnd;
+
+	@Column(name = "averageRating", nullable = true)
+	private float averageRating;
 
 	@JsonIgnore
-    @ManyToOne(fetch = FetchType.EAGER)
-    private Centre centre;
-    
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Centre centre;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<ReviewDoctor> reviews;
 
 	@JsonIgnore
 	@ManyToMany(fetch = FetchType.EAGER)
 	private List<Appointment> appointments;
-	
+
 	@OneToMany(fetch = FetchType.EAGER)
 	private List<Vacation> vacations;
 
+
+	/*-----------------------------------------
+	***CONSTRUCTORS
+	 ------------------------------------------*/
 
 
 	public Doctor() {
@@ -52,7 +57,7 @@ public class Doctor extends User
 	}
 
 	public Doctor(String username, String password, String email, String firstname, String lastname, String city,
-			 String state, String date_of_birth, String phone) {
+				  String state, String date_of_birth, String phone) {
 		super(username, password, email, firstname, lastname, city, state, date_of_birth, phone, UserRole.Doctor);
 		this.setIsFirstLog(true);
 		this.appointments = new ArrayList<>();
@@ -68,94 +73,52 @@ public class Doctor extends User
 		this.vacations = new ArrayList<Vacation>();
 		this.reviews = new ArrayList<ReviewDoctor>();
 	}
-	
+
 	public Doctor(DoctorDTO dto)
 	{
 		super(dto.getUser());
 		this.setRole(UserRole.Doctor);
 		this.setIsFirstLog(true);
-		this.shiftStart = DateUtil.getInstance().getDate(dto.getShiftStart(), "HH:mm");
-		this.shiftEnd = DateUtil.getInstance().getDate(dto.getShiftEnd(), "HH:mm");
+		this.type = dto.getType();
+		this.shiftStart = DateUtil.getInstance().getDate(dto.getShiftStart(), "yyyy-MM-dd HH:mm");
+		//DateUtil.getInstance().getDate(dto.getShiftStart(), "HH:mm");
+		this.shiftEnd =DateUtil.getInstance().getDate(dto.getShiftEnd(), "yyyy-MM-dd HH:mm");
+		//DateUtil.getInstance().getDate(dto.getShiftEnd(), "HH:mm");
+
 		this.appointments = new ArrayList<Appointment>();
 		this.vacations = new ArrayList<Vacation>();
 		this.reviews = new ArrayList<ReviewDoctor>();
 	}
-
+	/*---------------------------------------
+    ***METHODS
+     ----------------------------------------*/
 	public Boolean IsFreeOn(Date date)
 	{
 		for(Vacation v: vacations)
 		{
 			Date start = v.getStartDate();
 			Date end = v.getEndDate();
-			
+
 			if(date.after(start) && date.before(end))
 			{
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-		
-	public List<Vacation> getVacations() {
-		return vacations;
-	}
 
-	public void setVacations(List<Vacation> vacations) {
-		this.vacations = vacations;
-	}
-
-	public Date getShiftStart() {
-		return shiftStart;
-	}
-
-	public void setShiftStart(Date shiftStart) {
-		this.shiftStart = shiftStart;
-	}
-
-	public Date getShiftEnd() {
-		return shiftEnd;
-	}
-
-	public void setShiftEnd(Date shiftEnd) {
-		this.shiftEnd = shiftEnd;
-	}
-
-	public Centre getCentre() {
-		return centre;
-	}
-
-	public void setCentre(Centre centre) {
-		this.centre = centre;
-	}
-
-	public String getType() {
-		return type;
-	}
-	
-	public void setType(String type) {
-		this.type = type;
-	}
-			
-	public float getAverageRating() {
-		return averageRating;
-	}
-
-	public void setAverageRating(float averageRating) {
-		this.averageRating = averageRating;
-	}
-	
 	public float calculateRating()
 	{
 		List<ReviewDoctor> reviews = getReviews();
 		List<Integer> ratings = new ArrayList<Integer>();
 		float sum = 0;
-		
+
 		if(reviews.isEmpty())
 		{
 			return -1;
 		}
-		
+
 		for(ReviewDoctor cr : reviews)
 		{
 			if(cr.getRating() >= 0)
@@ -163,37 +126,22 @@ public class Doctor extends User
 				ratings.add(cr.getRating());
 			}
 		}
-	
+
 		for(Integer r : ratings)
 		{
 			sum = sum + r;
 		}
-		
-		 return sum = (Float) sum / ratings.size();
+
+		return sum = (Float) sum / ratings.size();
 	}
 
-	public List<ReviewDoctor> getReviews() {
-		return reviews;
-	}
-
-	public void setReviews(List<ReviewDoctor> reviews) {
-		this.reviews = reviews;
-	}
-
-	public List<Appointment> getAppointments() {
-		return appointments;
-	}
-
-	public void setAppointments(List<Appointment> appointments) {
-		this.appointments = appointments;
-	}
-	
+	//-------------------------------------------------------------
 	public static class Builder extends UserBuilder
 	{
 		private String type;
-	    private Date shiftStart;
-	    private Date shiftEnd;
-	    public Centre centre;
+		private Date shiftStart;
+		private Date shiftEnd;
+		public Centre centre;
 
 
 		protected Builder(String email) {
@@ -211,36 +159,36 @@ public class Doctor extends User
 		public Builder withPassword(String password)
 		{
 			super.withPassword(password);
-			
+
 			return this;
 		}
 
 		public Builder withFirstname(String firstname)
 		{
 			super.withFirstname(firstname);
-			
-			return this;
-		}
-	
-		public Builder withLastname(String lastname)
-		{
-			super.withLastname(lastname);
-			
-			return this;
-		}
-		
-		public Builder withCity(String city)
-		{
-			super.withCity(city);
-			
+
 			return this;
 		}
 
-		
+		public Builder withLastname(String lastname)
+		{
+			super.withLastname(lastname);
+
+			return this;
+		}
+
+		public Builder withCity(String city)
+		{
+			super.withCity(city);
+
+			return this;
+		}
+
+
 		public Builder withState(String state)
 		{
 			super.withState(state);
-			
+
 			return this;
 		}
 
@@ -250,44 +198,44 @@ public class Doctor extends User
 
 			return this;
 		}
-		
+
 		public Builder withPhone(String phone)
 		{
 			super.withPhone(phone);
-			
+
 			return this;
 		}
 
-		
+
 		public Builder withType(String type)
 		{
 			this.type = type;
-			
-			return this;
-		}
-		
-		public Builder withShiftStart(Date shiftStart)
-		{
-			this.shiftStart = shiftStart;
-			
-			return this;
-		}
-		
-		public Builder withShiftEnd(Date shiftEnd)
-		{
-			this.shiftEnd = shiftEnd;
-			
-			return this;
-		}
-		
-		public Builder withCentre(Centre centre)
-		{
-			this.centre = centre;
-			
+
 			return this;
 		}
 
-		
+		public Builder withShiftStart(Date shiftStart)
+		{
+			this.shiftStart = shiftStart;
+
+			return this;
+		}
+
+		public Builder withShiftEnd(Date shiftEnd)
+		{
+			this.shiftEnd = shiftEnd;
+
+			return this;
+		}
+
+		public Builder withCentre(Centre centre)
+		{
+			this.centre = centre;
+
+			return this;
+		}
+
+
 		public Doctor build()
 		{
 			super.withRole(UserRole.Doctor);

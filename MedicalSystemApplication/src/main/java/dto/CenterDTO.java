18e576f67ecab1ebc77e35bbe04202748688aba0
Denis <metelitsa.denis@gmail.com>
2023-04-22 21:22:34 +0300

    Add id to CenterDTO + сode refactoring

--- MedicalSystemApplication/src/main/java/dto/CenterDTO.java
+++ MedicalSystemApplication/src/main/java/dto/CenterDTO.java
@@ -2,9 +2,11 @@ package dto;
 
 import lombok.Data;
 import model.Center;
+
 @Data
 public class CenterDTO {
 
+    private Long id;
     private String name;
     private String address;
     private String city;
@@ -12,32 +14,18 @@ public class CenterDTO {
     private String description;
     private float rating;
 
-    public CenterDTO()
-    {
-    	super();
+    public CenterDTO() {
+        super();
     }
-    
-    public CenterDTO(String name, String address, String city, String state, String description, float rating) {
-        this.name = name;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.description = description;
-        this.rating = rating;
-    }
-    
-    public CenterDTO(Center center)
-    {
-    	this.name = center.getName();
+
+    public CenterDTO(Center center) {
+        this.id = center.getId();
+        this.name = center.getName();
         this.address = center.getAddress();
         this.city = center.getCity();
         this.state = center.getState();
         this.description = center.getDescription();
-        
         this.rating = center.calculateRating();
     }
 
-
-    
-    
 }
